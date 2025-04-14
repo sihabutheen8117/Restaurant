41fd6da800283c95504b23f1803bab56812483c9
sihabutheen8117 <sihabutheen8117@gmail.com>
2025-04-14 12:26:59 +0530

    navbar

--- client/src/components/Home/MainNav.tsx
+++ client/src/components/Home/MainNav.tsx
@@ -6,7 +6,7 @@ import '@/styles/mainStyles.css'
 const MainNav = () => {
   return (
     <nav className='m-5 flex justify-between'>
-        <div className={`${lexend.className} text-lg md:pl-10`}>
+        <div className={`${lexend.className} text-2xl md:pl-10`}>
             Al-Khalid
         </div>
         <div className='flex gap-8'>
@@ -15,11 +15,11 @@ const MainNav = () => {
             <div>certificates</div>
             <div>Order now</div>
         </div>
-        <div className='pr-5'>
-            <button className='pr-5'>
+        <div className='pr-5 text-lg'>
+            <button className='pr-5 font-semibold hover:text-neutral-600 fontrans'>
                 log in
             </button>
-            <button className={`vividOrange rounded-2xl py-0.5 px-3`}>
+            <button className={`lightYellow rounded-2xl font-semibold py-0.5 px-3 hover:text-neutral-600 fontrans`}>
                 Sign in
             </button>
         </div>
