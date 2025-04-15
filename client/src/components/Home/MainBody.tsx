e800924230fb74b26c8a608e2066576ca0b0e31a
sihabutheen8117 <sihabutheen8117@gmail.com>
2025-04-15 22:00:49 +0530

    infinite scrollling

--- client/src/components/Home/MainBody.tsx
+++ client/src/components/Home/MainBody.tsx
@@ -1,10 +1,23 @@
 
 import React from 'react'
 import '../../styles/mainStyles.css'
-import {lexend} from '@/utils/fonts'
+import {lexend ,kanit} from '@/utils/fonts'
+
 const MainBody = () => {
   return (
     <div className=''>
+      <div className={`flex justify-center font-semibold md:text-2xl md:mt-10 mt-5 ${kanit.className}`}>Certificates</div>
+      <div className='wrapper md:mt-10 mt-5 md:mx-14 mx-4 pt-[10px]'>
+        <div className='item item1 shadow-xl'> <img src="assets/certificates/fssai.png" alt='fssai' className={`w-full h-full object-contain`}/></div>
+        <div className='item item2 shadow-xl flex items-center '> <div className={`${lexend.className} md:text-2xl md:pl-10 text-lg`}>Al-Khalid</div></div>
+        <div className='item item3 shadow-xl'> <img src="assets/certificates/iso_white.png" alt='fssai' className={`w-full h-full object-contain`}/></div>
+        <div className='item item4'></div>
+        <div className='item item5'></div>
+        <div className='item item6'></div>
+        <div className='item item7'></div>
+        <div className='item item8'></div>
+        <div className="test test1"></div>
+      </div>
       <div className={`md:m-10 m-3 flex relative`}>
         {/* <img src='assets/top_view_food.jpg' alt='top view food' className='md:rounded-4xl rounded-2xl w-64 h-64 object-cover'/> */}
         <div className={`md:rounded-4xl rounded-2xl  absolute inset-0 setAbout bg-cover bg-right bg-no-repeat h-96 w-full z-0`}>
--- client/src/utils/fonts.ts
+++ client/src/utils/fonts.ts
@@ -1,5 +1,6 @@
 import { Lexend } from "next/font/google";
 import { Anton } from "next/font/google";
+import { Kanit } from "next/font/google";
 
 export const lexend = Lexend({
     subsets: ['latin'],
@@ -10,5 +11,11 @@ export const lexend = Lexend({
   export const anton = Anton({
     subsets: ['latin'],
     variable: '--font-anton',
+    weight: ['400'],
+  })
+
+  export const kanit = Kanit({
+    subsets: ['latin'],
+    variable: '--font-kanit',
     weight: ['400'],
   })
