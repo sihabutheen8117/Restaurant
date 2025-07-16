13837aaa0189db6eaea15cac549b160c57aec0ad
sihabutheen8117 <sihabutheen8117@gmail.com>
2025-07-16 10:15:35 +0530

    test-1-backend

--- server/src/server.mjs
+++ server/src/server.mjs
@@ -34,9 +34,9 @@ mongoose.connect("mongodb+srv://sihabutheen8117:<db_password>@cluster0.296mzuv.m
 const PORT = process.env.PORT ;
 
 // front-ends : http://localhost:3000
-// https://restaurant-ten-black.vercel.app/
+// https://restaurant-a9nobx1bv-sihabutheens-projects.vercel.app
 const corsOptions = {
-    origin : "https://restaurant-ten-black.vercel.app/" , 
+    origin : "https://restaurant-a9nobx1bv-sihabutheens-projects.vercel.app" , 
     methods : ['GET' , "POST" , 'PUT' , 'DELETE'], 
     credentials: true 
 }
@@ -48,7 +48,7 @@ app.use(cors(corsOptions));
 const server = http.createServer(app);
 const io = new Server(server ,{
     cors: {
-      origin: "https://restaurant-ten-black.vercel.app/", // Your Next.js app URL
+      origin: "https://restaurant-a9nobx1bv-sihabutheens-projects.vercel.app", // Your Next.js app URL
       methods: ["GET", "POST"],
       credentials: true
     }
