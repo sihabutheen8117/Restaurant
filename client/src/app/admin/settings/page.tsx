ea0a65e474931aeadc63e76e3c94b7f0786d54be
sihabutheen8117 <sihabutheen8117@gmail.com>
2025-07-23 20:52:30 +0530

    test-42-user-ui

--- client/src/app/admin/settings/page.tsx
+++ client/src/app/admin/settings/page.tsx
@@ -5,7 +5,7 @@ import NotificationLoader from '@/components/Loaders/NotificationLoader'
 const Page = () => {
   const [error, set_error] = useState(false)
   const [visible, set_visible] = useState(false)
-  const timeoutRef = useRef(null)
+  const timeoutRef = useRef(0)
 
   const handleSignin = () => {
     if (error && !visible) {
